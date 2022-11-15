68e814544d7d6c281630ee4ab2834c46e8c0d321
Luis Soares <luiszkm@gmail.com>
2022-11-14 22:36:40 -0300

    add new script

---
+++ script2/script-infra.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "att aplication "
+
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "download and copy aplicatio file"
+
+cd/tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+
+
+
